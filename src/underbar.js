381047f40e1553a30e3e62bee263dd7e43869a16
Vinoj <vinojgovi@gmail.com>
2017-09-16 20:55:04 -0700

    Finished last

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-  	
+    if (n === undefined) {
+      return array[array.length-1];
+  } else if (n>array.length) {
+      return array;
+  } else {
+      return array.slice(array.length-n);
+  }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
