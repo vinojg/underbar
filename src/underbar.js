83ff83b0f086bbe645b89e5cd015a32f81b1bd6e
Vinoj <vinojgovi@gmail.com>
2017-09-20 20:36:43 -0700

    Finished every

--- src/underbar.js
+++ src/underbar.js
@@ -209,6 +209,20 @@ _.reduce = function(collection, iterator, accumulator){
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function (wasFound, item){
+     if (!wasFound) {
+       return false
+     }
+     if (iterator===undefined) {
+       if (!item) {
+         return false;
+       }
+     } else {
+       if (!iterator(item)) {
+       return false;
+       }
+     }
+   }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
